3158b0ec42a7b6b09946ffd6421efb036f5810eb
Alex Marse <alexmarse@Alexs-MacBook-Pro-2.local>
2014-06-21 18:28:03 -0400

    Got osc server running in flask app

--- flaskapp/oscbridge/app.py
+++ flaskapp/oscbridge/app.py
@@ -19,6 +19,8 @@ def create_app(app_name=None):
     configure_app(app)
     configure_sockets(app)
     configure_routes(app)
+    configure_queue(app)
+    configure_osc_receiver()
     
     return app
 
@@ -35,6 +37,11 @@ def configure_app(app, configfile=None):
     # The Flask-common configurations are within the FLASK: definition
     app.config.update(app.settings.get('FLASK'))
 
+
+def configure_queue(app):
+    import Queue
+    with app.app_context():
+        app.queue = Queue.Queue()
 
 def configure_sockets(app):
     from flask_sockets import Sockets
@@ -55,9 +62,11 @@ def configure_routes(app):
     def kinect_stream():
         """Pass data from OSC receiver back through the socket
         """
-        pass
+        if not app.queue.empty():
+#             OSCWebSocketHandler.send_updates(json.dumps({"msg": queue.get() }))
+            print app.queue.get()
 
-def configure_osc_receiver(app):
+def configure_osc_receiver():
     """Set up the OSC receiver so that data can be sent to the websocket 
     """
     from .modules.osc import receiver
--- flaskapp/tests/osccli.py
+++ flaskapp/tests/osccli.py
@@ -8,4 +8,6 @@ client.send( OSCMessage("/user/2", [2.0, 3.0, 4.0 ] ) )
 client.send( OSCMessage("/user/3", [2.0, 3.0, 3.1 ] ) )
 client.send( OSCMessage("/user/4", [3.2, 3.4, 6.0 ] ) )
 
+client.send( OSCMessage("/test/1", [3.2, 3.4, 6.0, 'Testing' ] ) )
+
 client.send( OSCMessage("/quit") )
